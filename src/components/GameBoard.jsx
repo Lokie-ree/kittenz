920f47a8de438fe0ab1704eccc44a330535f396f
Lokie-ree <165349722+Lokie-ree@users.noreply.github.com>
2024-07-24 17:21:00 -0500

    first commit

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,13 +1,155 @@
-import React from 'react';
+'use client';
+
+import React, { useState, useEffect } from 'react';
 import KittenCard from './KittenCard';
+import { getRandomKittens } from '@/services';
+import Confetti from 'react-confetti';
 
 const GameBoard = () => {
-	const kittensArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+	const [numOfKittens, setNumOfKittens] = useState(6);
+	const [kittens, setKittens] = useState([]);
+	const [cardOne, setCardOne] = useState(null);
+	const [cardTwo, setCardTwo] = useState(null);
+	const [solvedKittensArray, setSolvedKittensArray] = useState([]);
+	const [loading, setLoading] = useState(true);
+	const [gameWin, setGameWin] = useState(false);
+
+  // New Game
+  const newGame = async () => {
+    setLoading(true);
+    setGameWin(false);
+    setKittens([]);
+    setCardOne(null);
+    setCardTwo(null);
+    setSolvedKittensArray([]);
+    await fetchKittens();
+    setLoading(false);
+  }
+
+	// Function to fetch kittens
+	const fetchKittens = async () => {
+		setLoading(true);
+		const response = await getRandomKittens(numOfKittens);
+		if (response.isError) {
+			alert(response.error);
+		} else {
+			console.log('API Reponse:', response.images);
+			//RANDOMIZE AND DUPLICATE KITTENS HERE
+			const duplicatedAndShuffledKittens = randomizeAndDuplicateKittens(
+				response.images
+			);
+			setKittens(duplicatedAndShuffledKittens);
+		}
+		setLoading(false);
+	};
+
+	// Function to duplicate and randomize kittens
+	const randomizeAndDuplicateKittens = (kittensArray) => {
+		console.log('Kittens Array: ', kittensArray);
+		let newArray = [];
+		kittensArray.forEach((kitten, index) => {
+			let kittenCopy1 = { ...kitten, id: index + 'copy1' };
+			let kittenCopy2 = { ...kitten, id: index + 'copy2' };
+
+			newArray.push(kittenCopy1, kittenCopy2);
+		});
+		console.log('Duplicated Kittens', newArray);
+		newArray = newArray.sort(() => Math.random() - 0.5);
+		console.log('Duplicated and Shuffled', newArray);
+		return newArray;
+	};
+
+	// Function to check matches
+	useEffect(() => {
+		if (cardTwo != null) {
+			const timeout = setTimeout(() => {
+				if (cardOne.url === cardTwo.url && cardOne.id != cardTwo.id) {
+					setSolvedKittensArray((prev) => [...prev, cardOne.url]);
+				}
+				setCardOne(null);
+				setCardTwo(null);
+			}, 500);
+			return () => clearTimeout(timeout);
+		}
+	}, [cardTwo]);
+
+	useEffect(() => {
+		newGame();
+	}, []);
+
+  useEffect(() => {
+    newGame();
+  }, [numOfKittens]);
+
+  useEffect(() => {
+    if(solvedKittensArray.length === numOfKittens) {
+      setGameWin(true);
+      document.getElementById('meow').play();
+    }
+  }, [solvedKittensArray]);
+
 	return (
-		<div className="flex flex-wrap flex-row gap-4 w-full justify-center">
-			{kittensArray.map((num) => (
-				<KittenCard number={num} />
-			))}
+		<div className="flex flex-wrap flex-col gap-4 w-full justify-center">
+      <audio id='meow' src='/cat-meow.mp3' />
+      {gameWin && (
+        <div className="fixed top-0 left-0 right-0 z-[100]">
+          <Confetti
+            confettiSource={{ x: 0, y: 50, w: window.innerWidth, h: 0}}
+            width={window.width}
+            height={window.height}
+            recycle={false}
+            numberOfPieces={1000}
+            // onConfettiComplete={() => setGameWin(false)}
+      />
+      </div>
+    )}
+      <div className="flex flex-row w-full items-center justify-between p-6">
+        <button 
+          onClick={() => newGame()}
+          className={`btn ${gameWin && 'animate-bounce'}`}>New Game</button>
+        <details 
+          className="dropdown dropdown-end" 
+          id='number-of-kittens-dropdown'
+        >
+          <summary className="btn m-1">Number of Kittens</summary>
+            <ul className="menu dropdown-content bg-base-300 rounded-box z-[1] w-52 p-2 shadow">
+              {[6, 8, 10, 12].map((number) => (
+                <li
+                  key={number}
+                  onClick={() => {
+                    setNumOfKittens(number)
+                    document
+                      .getElementById('number-of-kittens-dropdown')
+                      .removeAttribute('open')
+                    }}
+                >
+                <a>{number}</a>
+              </li>))}
+            </ul>
+        </details>
+      </div>
+
+      <div className="flex flex-row items-center justify-center flex-wrap gap-6 mb-12">
+        {loading ? (
+          <p className="text-xl flex items-center justify-center h-64">
+            loading<span className="loading loading-dots loading-lg"></span>
+          </p>
+        ) : (
+          kittens.map((kitten) => (
+            <KittenCard
+              key={kitten?.id}
+              kitten={kitten}
+              cardOne={cardOne}
+              setCardOne={setCardOne}
+              cardTwo={cardTwo}
+              setCardTwo={setCardTwo}
+              solvedKittensArray={solvedKittensArray}
+              setSolvedKittensArray={setSolvedKittensArray}
+            />
+          ))
+        )}
+      </div>
+			
 		</div>
 	);
 };
--- src/components/KittenCard.jsx
+++ src/components/KittenCard.jsx
@@ -1,10 +1,53 @@
 import React from 'react';
 
-const KittenCard = ({ number }) => {
+const KittenCard = ({
+	kitten,
+	cardOne,
+	cardTwo,
+	setCardOne,
+	setCardTwo,
+	solvedKittensArray,
+	setSolvedKittensArray,
+}) => {
+	const handleClick = () => {
+		if (cardOne === null) {
+			setCardOne(kitten);
+			console.log('card one', cardOne);
+			return;
+		}
+		if (cardTwo === null) {
+			setCardTwo(kitten);
+			console.log('card two', cardTwo);
+			return;
+		}
+	};
+
 	return (
-		<div className="flex h-80 w-80 items-center justify-center bg-secondary rounded-lg">
-			Click Me {number}
-		</div>
+		<button
+			onClick={handleClick}
+			disabled={cardOne && cardTwo}
+			className="flex h-80 w-80 items-center justify-center bg-secondary rounded-lg overflow-clip"
+		>
+			<p
+				className={`${
+					(cardOne == kitten ||
+						cardTwo === kitten ||
+						solvedKittensArray.includes(kitten.url)) &&
+					'hidden'
+				}`}
+			>
+				Click Me
+			</p>
+			<img
+				className={`${
+					cardOne != kitten &&
+					cardTwo != kitten &&
+					!solvedKittensArray.includes(kitten.url) &&
+					'hidden'
+				} object-cover w-full h-full`}
+				src={kitten.url}
+			/>
+		</button>
 	);
 };
 
